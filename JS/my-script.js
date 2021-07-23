d7cc12072ef6f99bf7c1744b7297bd1ed81717f6
vamandeshmukh <vamandeshmukh@gmail.com>
2021-07-23 16:43:32 +0530

    C

--- JS/my-script.js
+++ JS/my-script.js
@@ -4,7 +4,30 @@
 // Chrome - engine to run JS - v8 
 // Node - engine to run JS - v8 
 
-console.log("Hello world! 4");
-let num = 10;
-console.log(num);
+// introduction to JS 
+// console.log("Hello world! 4");
+// let num = 10;
+// console.log(num);
+
+// variable declaration 
+
+// Java -          int num = 10;
+// JavaScript -    let num = 10;
+// JavaScript -    var let const num = 10; 
+
+let num1 = 10;
+console.log(typeof(num1));
+console.log(num1);
+num1 = 'Sonu';
+console.log(typeof(num1));
+console.log(num1);
+num1 = true;
+console.log(typeof(num1));
+console.log(num1);
+
+
+
+
+
+
 
