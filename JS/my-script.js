88f78e2556e947dcb17f9cd420491a0f8bb0b622
vamandeshmukh <vamandeshmukh@gmail.com>
2021-07-23 16:10:12 +0530

    C

--- JS/my-script.js
+++ JS/my-script.js
@@ -1,6 +1,10 @@
 
 // HTML 5 CSS 3 JS 6  
+// Edge - engine to run JS - chakra 
+// Chrome - engine to run JS - v8 
+// Node - engine to run JS - v8 
 
+console.log("Hello world! 4");
 let num = 10;
 console.log(num);
 
